5d660f46fbc5a751295ff7241abc953e33510053
ponchogrill <114858013+ponchogrill@users.noreply.github.com>
2022-11-06 13:21:21 +0300

    Add files via upload

---
+++ spisok.py
@@ -0,0 +1,64 @@
+##print("Введите размер списка")
+##n = int(input())
+##lst = []
+##for i in range(n):
+##    print("Введите число")
+##    x = int(input())
+##    lst.append(x)
+##for i in range(n):
+##    if lst[i] % 2 == 0:
+##        print(lst[i], end = " ")
+    
+##print("Введите размер списка")
+##n = int(input())
+##lst = []
+##for i in range(n):
+##    print("Введите число")
+##    x = int(input())
+##    lst.append(x)
+##    s = 0
+##for i in range(n):
+##    s += lst[i]
+##    print(s, end = " ")
+    
+##print("Введите размер списка")
+##n = int(input())
+##lst = []
+##for i in range(n):
+##    print("Введите число")
+##    x = int(input())
+##    lst.append(x)
+##    p = 1
+##for i in range(n):
+##    p *= lst[i]
+##    print(p, end = " ")
+
+##print("Введите размер списка")
+##
+##n = int(input())
+##lst = []
+##for i in range(n):
+##    print("Введите число")
+##    x = int(input())
+##    lst.append(x)
+##
+##s = 0
+##for i in range(n):
+##    s + lst[i]
+##    
+##print(s, end = " ")
+
+print("Введите размер списка")
+
+n = int(input())
+lst = []
+for i in range(n):
+    print("Введите число")
+    x = int(input())
+    lst.append(x)
+
+s = 0
+for i in range(n):
+    s + lst[i]
+    
+print(s, end = " ")
